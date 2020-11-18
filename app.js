68645048e07e5e6c58ea8429574059c19e5309e2
Mathew180 <mathewasuku400gmail.com>
2020-11-18 19:04:23 +0100

    project

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
+
 openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
+  button.addEventListener('click' , () => {
     const modal = document.querySelector(button.dataset.modalTarget)
     openModal(modal)
   })
 })
-
+  
 overlay.addEventListener('click', () => {
   const modals = document.querySelectorAll('.modals.active')
   modals.forEach(modal => {
